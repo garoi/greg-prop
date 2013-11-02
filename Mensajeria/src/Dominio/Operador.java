cea55f1f1247c566648da79870fd37064e91c60f
garoi <marc.garcia.roig@gmail.com>
2013-11-02 19:48:12 +0100

    ControlDominio hecho, pero falta poner la optimizacion

--- Mensajeria/src/Dominio/Operador.java
+++ Mensajeria/src/Dominio/Operador.java
@@ -1,5 +1,7 @@
 package Dominio;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author ivich
@@ -8,6 +10,11 @@ public class Operador {
     private String nombre = new String();
     private int IDenc;
     
+    //DEvuelve un vector con el identificador de los destinos de los paquetes seleccionados
+    public int[] SeleccionarDestinos() {
+        int[] PaquetesDestino = null;
+        return PaquetesDestino;
+    }
     
     
 }
